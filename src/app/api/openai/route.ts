3f45db6f29e693c8d0450b64ab203b10ad5e727e
eunwoo-levi <eunwoo1341@gmail.com>
2024-11-29 16:18:39 +0900

    feat: implement chatbot client component

--- src/app/api/openai/route.ts
+++ src/app/api/openai/route.ts
@@ -10,13 +10,14 @@ export async function POST(req: Request) {
     const body = await req.json();
     
     const completion = await openai.chat.completions.create({
-      model: "GPT-3.5 Turbo",
+      model: "gpt-4o-mini",
       messages: [
-        { role: "system", content: "you are my kind girlfriend" },
+        { role: "system", content: "you are my kind girlfriend. if I talk to you in Korean, talk to me in Korean in friendly way. respond to me in casual text style." },
         { role: "user", content: body.message }
       ]
     });
 
+    console.log('테스트',completion)
     return NextResponse.json({ result: completion.choices[0].message });
   } catch (error) {
     return NextResponse.json({ error: 'Error occurred' }, { status: 500 });
